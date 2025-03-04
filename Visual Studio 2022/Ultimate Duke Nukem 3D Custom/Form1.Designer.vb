1ce9f187880b3717854333816b727c3996e35d6c
y0uls <55883556+y0uls@users.noreply.github.com>
2025-03-04 14:13:56 +0100

    Add files via upload

---
+++ Visual Studio 2022/Ultimate Duke Nukem 3D Custom/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ButtonExit = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBoxFilter = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.ButtonOpenFolder = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(32, 328)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(122, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start Game"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(32, 36)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(150, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(32, 111)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(312, 173)
+        Me.ListBox1.TabIndex = 4
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.BackColor = System.Drawing.Color.Transparent
+        Me.CheckBox1.ForeColor = System.Drawing.Color.Yellow
+        Me.CheckBox1.Location = New System.Drawing.Point(32, 290)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(87, 17)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "Server Mode"
+        Me.CheckBox1.UseVisualStyleBackColor = False
+        '
+        'ButtonExit
+        '
+        Me.ButtonExit.Location = New System.Drawing.Point(269, 328)
+        Me.ButtonExit.Name = "ButtonExit"
+        Me.ButtonExit.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonExit.TabIndex = 6
+        Me.ButtonExit.Text = "Exit"
+        Me.ButtonExit.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(219, 85)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(125, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'ComboBoxFilter
+        '
+        Me.ComboBoxFilter.FormattingEnabled = True
+        Me.ComboBoxFilter.Location = New System.Drawing.Point(32, 84)
+        Me.ComboBoxFilter.Name = "ComboBoxFilter"
+        Me.ComboBoxFilter.Size = New System.Drawing.Size(122, 21)
+        Me.ComboBoxFilter.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.Yellow
+        Me.Label1.Location = New System.Drawing.Point(29, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Type of game"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.ForeColor = System.Drawing.Color.Yellow
+        Me.Label2.Location = New System.Drawing.Point(29, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Filter"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.ForeColor = System.Drawing.Color.Yellow
+        Me.Label3.Location = New System.Drawing.Point(216, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Autocomplete"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.BackColor = System.Drawing.Color.Transparent
+        Me.CheckBox2.ForeColor = System.Drawing.Color.Yellow
+        Me.CheckBox2.Location = New System.Drawing.Point(126, 291)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(74, 17)
+        Me.CheckBox2.TabIndex = 12
+        Me.CheckBox2.Text = "Fullscreen"
+        Me.CheckBox2.UseVisualStyleBackColor = False
+        '
+        'ButtonOpenFolder
+        '
+        Me.ButtonOpenFolder.Location = New System.Drawing.Point(219, 36)
+        Me.ButtonOpenFolder.Name = "ButtonOpenFolder"
+        Me.ButtonOpenFolder.Size = New System.Drawing.Size(101, 23)
+        Me.ButtonOpenFolder.TabIndex = 13
+        Me.ButtonOpenFolder.Text = "Browse Folder"
+        Me.ButtonOpenFolder.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Ultimate_Duke_Nukem_3D_Custom.My.Resources.Resources.yrbaxqxyrfj0rapcsscr
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(380, 380)
+        Me.Controls.Add(Me.ButtonOpenFolder)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBoxFilter)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ButtonExit)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Ultimate Duke Nukem 3D Custom"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents ButtonExit As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBoxFilter As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents ButtonOpenFolder As Button
+End Class
---
+++ Visual Studio 2022/Ultimate Duke Nukem 3D Custom/Form1.vb
@@ -0,0 +1,233 @@
+﻿Public Class Form1
+    Private mapsDir As String = "C:\Program Files (x86)\Ultimate Duke Nukem 3D Custom\CustomDuke\maps"
+    Private modsDir As String = "C:\Program Files (x86)\Ultimate Duke Nukem 3D Custom\CustomDuke\mods"
+    Private eduke32Path As String = "C:\Program Files (x86)\Ultimate Duke Nukem 3D Custom\CustomDuke\eduke32.exe"
+    Private specificFilePath As String = "C:\Program Files (x86)\Ultimate Duke Nukem 3D Custom\CustomDuke\fullscreen.cfg"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Empêcher le redimensionnement
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+
+        ComboBox1.Items.Add("Choose a custom map")
+        ComboBox1.Items.Add("Choose a custom mod")
+        ComboBox1.SelectedIndex = -1 ' Ne rien sélectionner par défaut
+        For i As Integer = Asc("A"c) To Asc("Z"c)
+            ComboBoxFilter.Items.Add(Chr(i))
+        Next
+        ComboBoxFilter.Items.Add("All")
+        ComboBoxFilter.SelectedIndex = ComboBoxFilter.Items.Count - 1 ' Select "All" by default
+        Console.WriteLine("Form loaded and ComboBox items added.")
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Select Case ComboBox1.SelectedIndex
+            Case 0
+                Console.WriteLine("Maps option selected.")
+                LoadMaps()
+                CheckBox1.Enabled = False ' Désactiver la CheckBox
+            Case 1
+                Console.WriteLine("Mods option selected.")
+                LoadMods()
+                CheckBox1.Enabled = True ' Activer la CheckBox
+        End Select
+    End Sub
+
+    Private Sub LoadMaps()
+        ListBox1.Items.Clear()
+        If IO.Directory.Exists(mapsDir) Then
+            Console.WriteLine("Maps directory exists.")
+            Dim folders = IO.Directory.GetDirectories(mapsDir)
+            For Each folder In folders
+                ListBox1.Items.Add(IO.Path.GetFileName(folder))
+                Console.WriteLine($"Map added: {IO.Path.GetFileName(folder)}")
+            Next
+        Else
+            MessageBox.Show("Maps directory does not exist.")
+            Console.WriteLine("Maps directory does not exist.")
+        End If
+    End Sub
+
+    Private Sub LoadMods()
+        ListBox1.Items.Clear()
+        If IO.Directory.Exists(modsDir) Then
+            Console.WriteLine("Mods directory exists.")
+            Dim folders = IO.Directory.GetDirectories(modsDir)
+            For Each folder In folders
+                ListBox1.Items.Add(IO.Path.GetFileName(folder))
+                Console.WriteLine($"Mod added: {IO.Path.GetFileName(folder)}")
+            Next
+        Else
+            MessageBox.Show("Mods directory does not exist.")
+            Console.WriteLine("Mods directory does not exist.")
+        End If
+    End Sub
+
+    Private Sub ComboBoxFilter_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxFilter.SelectedIndexChanged
+        FilterList()
+    End Sub
+
+    Private Sub TextBoxSearch_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        FilterList()
+    End Sub
+
+    Private Sub FilterList()
+        If ComboBox1.SelectedIndex = -1 Then
+            Return ' Ne rien faire si aucun élément n'est sélectionné dans ComboBox1
+        End If
+
+        Dim searchText As String = TextBox1.Text.ToLower()
+        Dim filterLetter As String = If(ComboBoxFilter.SelectedItem.ToString() = "All", "", ComboBoxFilter.SelectedItem.ToString().ToLower())
+        Dim items As List(Of String) = If(ComboBox1.SelectedIndex = 0, GetMapItems(), GetModItems())
+        ListBox1.Items.Clear()
+        For Each item In items
+            If item.ToLower().Contains(searchText) AndAlso (filterLetter = "" OrElse item.ToLower().StartsWith(filterLetter)) Then
+                ListBox1.Items.Add(item)
+            End If
+        Next
+    End Sub
+
+    Private Function GetMapItems() As List(Of String)
+        Dim items As New List(Of String)
+        If IO.Directory.Exists(mapsDir) Then
+            Dim folders = IO.Directory.GetDirectories(mapsDir)
+            For Each folder In folders
+                items.Add(IO.Path.GetFileName(folder))
+            Next
+        End If
+        Return items
+    End Function
+
+    Private Function GetModItems() As List(Of String)
+        Dim items As New List(Of String)
+        If IO.Directory.Exists(modsDir) Then
+            Dim folders = IO.Directory.GetDirectories(modsDir)
+            For Each folder In folders
+                items.Add(IO.Path.GetFileName(folder))
+            Next
+        End If
+        Return items
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ListBox1.SelectedItem IsNot Nothing Then
+            Dim selectedFolder As String = ListBox1.SelectedItem.ToString()
+            ' Démarrer le jeu avec le dossier sélectionné
+            If ComboBox1.SelectedIndex = 0 Then
+                StartGameWithMap(selectedFolder)
+            ElseIf ComboBox1.SelectedIndex = 1 Then
+                StartGameWithMod(selectedFolder)
+            End If
+        Else
+            MessageBox.Show("Please select a map or mod.")
+            Console.WriteLine("No map or mod selected.")
+        End If
+    End Sub
+
+    Private Sub StartGameWithMap(selectedFolder As String)
+        Dim fullPath As String = IO.Path.Combine(mapsDir, selectedFolder)
+        Dim mode As String = If(CheckBox1.Checked, "-server", String.Empty)
+        Dim specificFile As String = If(CheckBox2.Checked, $"-cfg ""{specificFilePath}""", String.Empty)
+        If IO.Directory.Exists(fullPath) Then
+            Dim mapFiles = IO.Directory.GetFiles(fullPath, "*.map").ToArray()
+            Dim count As Integer = mapFiles.Length
+
+            If count = 0 Then
+                Process.Start(New ProcessStartInfo(eduke32Path, $"-j""{fullPath}"" {mode} {specificFile} -usecwd") With {.WorkingDirectory = "C:\Program Files (x86)\Ultimate Duke Nukem 3D Custom\CustomDuke"})
+                Console.WriteLine($"Starting eduke32.exe with no specific map in {fullPath}")
+            ElseIf count = 1 Then
+                Process.Start(New ProcessStartInfo(eduke32Path, $"-j""{fullPath}"" ""{IO.Path.GetFileName(mapFiles(0))}"" {mode} {specificFile} -usecwd") With {.WorkingDirectory = "C:\Program Files (x86)\Ultimate Duke Nukem 3D Custom\CustomDuke"})
+                Console.WriteLine($"Starting eduke32.exe with map {mapFiles(0)} in {fullPath}")
+            Else
+                ' If there are multiple maps, let the user choose one
+                Dim mapFile As String = ChooseMap(mapFiles)
+                If Not String.IsNullOrEmpty(mapFile) Then
+                    Process.Start(New ProcessStartInfo(eduke32Path, $"-j""{fullPath}"" ""{IO.Path.GetFileName(mapFile)}"" {mode} {specificFile} -usecwd") With {.WorkingDirectory = "C:\Program Files (x86)\Ultimate Duke Nukem 3D Custom\CustomDuke"})
+                    Console.WriteLine($"Starting eduke32.exe with map {mapFile} in {fullPath}")
+                End If
+            End If
+        Else
+            MessageBox.Show("Selected map folder does not exist.")
+            Console.WriteLine("Selected map folder does not exist.")
+        End If
+    End Sub
+
+    Private Function ChooseMap(mapFiles As String()) As String
+        ' Display a dialog to choose a map if there are multiple maps
+        Dim mapChoiceForm As New Form With {
+            .Text = "Choose a Map",
+            .Size = New Size(300, 200)
+        }
+        Dim listBox As New ListBox With {
+            .Dock = DockStyle.Fill,
+            .SelectionMode = SelectionMode.One
+        }
+        listBox.Items.AddRange(mapFiles)
+        mapChoiceForm.Controls.Add(listBox)
+        Dim result As DialogResult = mapChoiceForm.ShowDialog()
+        If result = DialogResult.OK AndAlso listBox.SelectedItem IsNot Nothing Then
+            Return listBox.SelectedItem.ToString()
+        End If
+        Return String.Empty
+    End Function
+
+    Private Sub StartGameWithMod(selectedFolder As String)
+        Dim fullPath As String = IO.Path.Combine(modsDir, selectedFolder)
+        Dim mode As String = If(CheckBox1.Checked, "-server", String.Empty)
+        Dim specificFile As String = If(CheckBox2.Checked, $"-cfg ""{specificFilePath}""", String.Empty)
+
+
+        If IO.Directory.Exists(fullPath) Then
+            Dim modFiles = IO.Directory.GetFiles(fullPath, "*.pk3").Concat(IO.Directory.GetFiles(fullPath, "*.dat")).ToArray()
+            Dim count As Integer = modFiles.Length
+
+            If count = 1 Then
+                Process.Start(New ProcessStartInfo(eduke32Path, $"-j""{fullPath}"" ""{IO.Path.GetFileName(modFiles(0))}"" {mode} {specificFile} -usecwd") With {.WorkingDirectory = "C:\Program Files (x86)\Ultimate Duke Nukem 3D Custom\CustomDuke"})
+                Console.WriteLine($"Starting eduke32.exe with mod {modFiles(0)} in {fullPath}")
+
+
+
+            ElseIf IO.File.Exists(IO.Path.Combine(fullPath, "launcher.bat")) Then
+                Process.Start(New ProcessStartInfo(IO.Path.Combine(fullPath, "launcher.bat"), mode) With {.WorkingDirectory = fullPath})
+                Console.WriteLine($"Starting launcher.bat in {fullPath}")
+            ElseIf IO.File.Exists(IO.Path.Combine(fullPath, "eduke32.exe")) Then
+                Process.Start(New ProcessStartInfo(IO.Path.Combine(fullPath, "eduke32.exe"), $"-j""{fullPath}"" {mode} {specificFile} -usecwd") With {.WorkingDirectory = fullPath})
+                Console.WriteLine($"Starting eduke32.exe in {fullPath}")
+            Else
+                Process.Start(New ProcessStartInfo(eduke32Path, $"-j""{fullPath}"" {mode} {specificFile} -usecwd") With {.WorkingDirectory = "C:\Program Files (x86)\Ultimate Duke Nukem 3D Custom\CustomDuke"})
+                Console.WriteLine($"Starting eduke32.exe with mod in {fullPath}")
+            End If
+        Else
+            MessageBox.Show("Selected mod folder does not exist.")
+            Console.WriteLine("Selected mod folder does not exist.")
+        End If
+    End Sub
+
+    Private Sub ButtonExit_Click(sender As Object, e As EventArgs) Handles ButtonExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        ' Code pour gérer l'événement CheckBox1_CheckedChanged
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        ' Code pour gérer l'événement CheckBox2_CheckedChanged
+    End Sub
+
+    Private Sub ButtonOpenFolder_Click(sender As Object, e As EventArgs) Handles ButtonOpenFolder.Click
+        Dim selectedFolder As String = String.Empty
+
+        If ComboBox1.SelectedIndex = 0 Then
+            selectedFolder = mapsDir
+        ElseIf ComboBox1.SelectedIndex = 1 Then
+            selectedFolder = modsDir
+        End If
+
+        If Not String.IsNullOrEmpty(selectedFolder) AndAlso IO.Directory.Exists(selectedFolder) Then
+            Process.Start("explorer.exe", selectedFolder)
+        Else
+            MessageBox.Show("Please select a valid option from the type of game.")
+        End If
+    End Sub
+End Class
